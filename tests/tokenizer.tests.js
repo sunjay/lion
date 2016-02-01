f017a8f13833e1b4b69ec4da8c5c5fcec197ab17
Sunjay Varma <varma.sunjay@gmail.com>
2016-01-31 23:57:21 -0500

    Made all empty tests fail

--- tests/tokenizer.tests.js
+++ tests/tokenizer.tests.js
@@ -4,25 +4,32 @@ const Tokenizer = require('../src/tokenizer');
 
 describe('Tokenizer', () => {
   it('ignores whitespace at the beginning of an expression', () => {
+    expect(1).to.equal(2);
   });
 
   it('uses exclusively whitespace to group tokens', () => {
     // x^2 != x ^ 2
+    expect(1).to.equal(2);
   });
 
   it('allows the use of parenthesis with or without whitespace', () => {
+    expect(1).to.equal(2);
   });
 
   it('allows the use of backslash with or without whitespace', () => {
+    expect(1).to.equal(2);
   });
 
   it('accepts all characters valid for symbols', () => {
+    expect(1).to.equal(2);
   });
 
   it('rejects all characters invalid for symbols', () => {
+    expect(1).to.equal(2);
   });
 
   it('it accepts all basic operators as valid symbols', () => {
     // +, -, *, /, ^, ++, --, ==, >=, <=
+    expect(1).to.equal(2);
   });
 });
