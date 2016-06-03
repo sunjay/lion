66a4656548d11b0b45750563c95ffbe057df80bd
Sunjay Varma <varma.sunjay@gmail.com>
2016-06-03 00:30:03 -0400

    Modelling the grammar in data structures

--- src/parser.rs
+++ src/parser.rs
@@ -1,26 +1,50 @@
 use token::Token;
 use tokenizer::Tokenizer;
 
-#[derive(Debug, PartialEq)]
-pub enum ASTNode {
-    Statement {
-        children: Vec<Box<ASTNode>>
+type Program = Vec<Statement>;
+
+#[derive(Debug)]
+pub enum Statement {
+    NamedFunction {
+        name: String,
+        definition: Function,
     },
-    Function {
-        name: Option<String>,
-        parameters: Vec<Token>,
-        body: Box<ASTNode>,
+    AnonymousFunction(Function),
+    Assignment {
+        name: String,
+        expr: Expr,
     },
-    StringLiteral {
-        value: Token,
-    },
-    Term {
-        token: Token,
-    },
+    Expression(Expr),
+}
+
+pub type Expr = Vec<ExprItem>;
+
+#[derive(Debug)]
+pub enum ExprItem {
+    AnonymousFunction(Function),
+    SingleTerm(Term),
+    Group(Expr),
+}
+
+#[derive(Debug)]
+pub struct Function {
+    params: Vec<String>,
+    body: Expr,
+}
+
+#[derive(Debug)]
+pub enum Term {
+    Symbol(String),
+    Number(f64),
+    StringLiteral(String),
 }
 
 pub struct Parser {
     lexer: Tokenizer,
+}
+
+pub enum ParseError {
+    SyntaxError,
 }
 
 impl Parser {
@@ -30,8 +54,8 @@ impl Parser {
         }
     }
 
-    pub fn parse() -> Result<ASTNode, ()> {
-        Err(())
+    pub fn parse(&mut self) -> Result<Program, ParseError> {
+        Err(ParseError::SyntaxError)
     }
 }
 
