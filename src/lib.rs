0d6ae1b9bc76e460c3ad29b1280609d428044583
Sunjay Varma <varma.sunjay@gmail.com>
2016-06-04 02:20:22 -0400

    Exposing the proper modules in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
-#![cfg_attr(test, allow(dead_code))]
-
 #[macro_use]
 extern crate lazy_static;
 
 mod scanner;
 mod token;
 mod tokenizer;
-mod ast;
-mod parser;
 
-//TODO: Make this not public
-pub use parser::Parser;
+pub mod ast;
+pub mod parser;
 
