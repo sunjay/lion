017627eeee1daf773b5e8a04627a326af5365213
Sunjay Varma <varma.sunjay@gmail.com>
2016-06-06 03:16:52 -0400

    Commenting on what prelude is

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,3 +1,8 @@
+/// The definitions for every function used in prelude
+/// "The built in functions"
+/// These special functions allow for things like defining operators,
+/// performing basic operations and special math functions
+
 use rich_number::RichNumber;
 use eval_context::EvalContext;
 
