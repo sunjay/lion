94be9ebdc8948ef97202ae5ea297acc128e7bf24
Sunjay Varma <varma.sunjay@gmail.com>
2016-06-08 04:44:49 -0400

    Converting expressions to EvalTreeNodes

--- src/eval_context.rs
+++ src/eval_context.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use ast::{Function, Expr, Statement};
+use ast::{Function, Expr, ExprItem, Term, Statement};
 use rich_number::RichNumber;
 use prelude::setup_prelude;
 
@@ -14,6 +14,21 @@ pub enum EvalError {
 }
 
 pub type EvalResult = Result<ContextItem, EvalError>;
+
+#[derive(Debug, Clone)]
+struct EvalTreeNode {
+    item: ContextItem,
+    children: Vec<EvalTreeNode>,
+}
+
+impl EvalTreeNode {
+    fn new(item: ContextItem) -> EvalTreeNode {
+        EvalTreeNode {
+            item: item,
+            children: Vec::new(),
+        }
+    }
+}
 
 #[derive(Eq, PartialEq, Debug, Clone)]
 pub enum Fixity {
@@ -133,7 +148,56 @@ impl EvalContext {
     }
 
     fn evaluate(&mut self, expr: Expr) -> EvalResult {
+        let root = try!(self.build_eval_tree(expr));
+
         unimplemented!();
+    }
+
+    /// Builds an evaluation tree and returns the root node
+    fn build_eval_tree(&self, expr: Expr) -> Result<EvalTreeNode, EvalError> {
+        let nodes = try!(self.convert_to_nodes(expr));
+
+        unimplemented!();
+    }
+
+    fn convert_to_nodes(&self, expr: Expr) -> Result<Vec<EvalTreeNode>, EvalError> {
+        let mut nodes: Vec<EvalTreeNode> = Vec::new();
+
+        for item in expr.into_iter() {
+            nodes.push(match item {
+                ExprItem::AnonymousFunction(function) => {
+                    EvalTreeNode::new(ContextItem::Definition {
+                        precedence: FUNCTION_PRECEDENCE,
+                        fixity: Fixity::Prefix,
+                        function: function,
+                    })
+                },
+                ExprItem::SingleTerm(term) => match term {
+                    Term::Symbol(sym) => {
+                        let item = self.get(&sym);
+                        if item.is_none() {
+                            return Err(EvalError::UnknownSymbol(sym));
+                        }
+                        EvalTreeNode::new(item.unwrap())
+                    },
+                    Term::Number(num) => {
+                        EvalTreeNode::new(
+                            ContextItem::Number(
+                                RichNumber::from(num)
+                            )
+                        )
+                    },
+                    Term::StringLiteral(value) => {
+                        EvalTreeNode::new(
+                            ContextItem::Constant(value)
+                        )
+                    },
+                },
+                ExprItem::Group(expr) => try!(self.build_eval_tree(expr)),
+            });
+        }
+
+        Ok(nodes)
     }
 }
 
