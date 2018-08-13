f1b175f95d28fb52f745893e54524bb5ab5b7fbe
Sunjay Varma <varma.sunjay@gmail.com>
2018-08-12 22:13:15 -0600

    Printing out syntax error if one occurs

--- src/bin/lion.rs
+++ src/bin/lion.rs
@@ -1,11 +1,18 @@
 extern crate lion;
+extern crate nom;
 
-use lion::parser::parse_program;
+use nom::{Err::Error, Context::Code};
+use lion::parser::{parse_program, Span};
 use lion::context::Context;
 
 fn main() {
     let input = include_str!("../../examples/units.lion");
-    let program = parse_program(input).unwrap();
+    let program = match parse_program(input) {
+        Ok(program) => program,
+        Err(Error(Code(Span { line, .. }, _))) =>
+            panic!("Syntax Error: Line {}: {}", line, input.lines().nth(line as usize).unwrap()),
+        Err(_) => unreachable!(),
+    };
 
     let mut context = Context::default();
     context.walk_decls(&program);
